6b46cb7291f84afaa1852764005ca1fed2685c6f
wind_flowing <87889501+windllsnow@users.noreply.github.com>
2022-01-01 22:33:34 +0800

    Add files via upload

--- the_ulitimate/script.js
+++ the_ulitimate/script.js
@@ -59,3 +59,67 @@ if (twitter == "@KalobTaulien"){
 //typeof(____)
 //強制轉換 型態   一樣
 // 如 Number("123")
+
+//template  literal 字串
+
+let lst = [1,"two",[3],"four",5.11];
+lst.push(1);
+console.log(lst);
+let lstgg = lst.pop();
+console.log(lstgg);
+console.log(lst);
+
+
+
+//i從0開始， 是否 i <= lst，是 i+1，不是 跳 出去
+for(let i = 0; i <= lst.length;i++){
+    console.log(i,lst[i]); 
+
+}
+// const kk =document.querySelectorAll(".thisclass");
+// console.log(kk);//啊知，可能是沒載 jQuery
+//___________________________________________________________
+// const elements = document.querySelectorAll("li");
+// elements.forEach(node => {
+//     node.innerText = "this is a changed";
+// });
+//_________________________________________________________
+// elements.forEach((node, index) => {
+//     node.innerText = `Hello this is item ${index+1}`
+// });
+//___________________________________________________________
+// elements.forEach(node => {
+//     node.classList.add("custom-class", "secondary-class") ;
+// });
+//___________________________________________________________
+function AddNumbers(num1,num2){
+    
+    const total1 =Number(num1)+Number(num2);
+    return "an answers"+"___"+ total1;
+}
+const total = AddNumbers(1,"5.5");
+console.log(total);
+
+
+function hehe(Name,...numbers){
+    let total11=0
+
+    for (index in numbers){
+        console.log(numbers[index]);//依序印出  每個值
+        total11 = total11 +numbers[index];
+    }
+
+    return `${Name}: the total is ${total11}`;  //函數 回傳  加總
+} 
+const newTotal = hehe("Peter",1,4,7,13)
+console.log(newTotal);
+
+const person = {
+    'name' : 'John',
+    'age' : 18,
+    'height' : 280,
+    'speak' : function(want ="cookies") {
+        console.log(`Meow, I want the ${want}`);
+    }
+}
+person.speak();
