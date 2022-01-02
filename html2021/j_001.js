bd8429aca22955c7fd52e3014302be2eb250667e
wind_flowing <87889501+windllsnow@users.noreply.github.com>
2022-01-02 21:19:02 +0800

    Add files via upload

---
+++ html2021/j_001.js
@@ -0,0 +1,84 @@
+// console.log("hello  ! your message !");
+// alert("小心!  請 leave!");
+// prompt("Enter your message___")
+
+
+console.log(123);
+let x = 123;
+console.log(x);
+const y = 321;
+console.log(y);
+
+// let name = prompt ("Enter your name:");
+// alert ("Hello,_ " + name + "__ wellcome ");
+
+console.log(10 + 2 );
+console.log(10 - 3 );
+console.log( 10*3);
+console.log(10 /3);
+
+console.log(10%3);
+
+console.log(10**3);
+
+let z = 1  ;
+z++;
+console.log(z);
+
+z+=4;10
+
+console.log(z);
+
+//1加到 n 之 總和
+// let number =prompt ("Enter one number:  ");
+// let total=0
+// for (let i = 0; i<=number ;i){
+//         total +=i;
+//         i++;
+// }
+// console.log(total);
+
+
+let n1 = 10;
+let n2 = 20;
+let name = "Jet";
+let n3 = 30;
+let n4 = 40;
+//30Jet30404
+
+console.log(n1+n2+name+n3+n4);
+
+console.log("--------------------------------");
+
+let age = 21;
+console.log(age.toString()+10);
+let pi = 3.1415926535;
+console.log(pi.toFixed(5));
+
+console.log("---------------/-----------------");
+
+let name1 = "Wilson loves to eat noodles.";
+console.log(name1.length);
+console.log(name1[5]);
+console.log(name[100]);
+console.log(name1.slice(1,27));//1~26  (0~27) 故 頭尾都少1
+console.log(name1.indexOf("o"));
+console.log(name1.toUpperCase().indexOf("E"));
+console.log(name1.split(" "));
+console.log(name1.split(""));
+console.log("---------------//----------------");
+
+
+    
+let yourAge = prompt ("Enter  your age , please!");
+
+if (yourAge <= 12) {
+
+    alert("你的第一張兒童票 100 元");
+}else if((yourAge >12) &&( yourAge <= 65)) {
+    alert("你的票價是 350 元");
+}else{
+    alert("你的票價是 0 元");
+}
+
+console.log("---------------//----------------");
