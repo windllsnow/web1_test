d8bbc3809b42d7fdf79758e903300d91edb74d31
wind_flowing <87889501+windllsnow@users.noreply.github.com>
2022-01-08 20:41:31 +0800

    Add files via upload

--- html2021/j_001.js
+++ html2021/j_001.js
@@ -2,7 +2,6 @@
 // alert("小心!  請 leave!");
 // prompt("Enter your message___")
 
-
 console.log(123);
 let x = 123;
 console.log(x);
@@ -12,20 +11,21 @@ console.log(y);
 // let name = prompt ("Enter your name:");
 // alert ("Hello,_ " + name + "__ wellcome ");
 
-console.log(10 + 2 );
-console.log(10 - 3 );
-console.log( 10*3);
-console.log(10 /3);
+console.log(10 + 2);
+console.log(10 - 3);
+console.log(10 * 3);
+console.log(10 / 3);
 
-console.log(10%3);
+console.log(10 % 3);
 
-console.log(10**3);
+console.log(10 ** 3);
 
-let z = 1  ;
+let z = 1;
 z++;
 console.log(z);
 
-z+=4;10
+z += 4;
+10;
 
 console.log(z);
 
@@ -38,7 +38,6 @@ console.log(z);
 // }
 // console.log(total);
 
-
 let n1 = 10;
 let n2 = 20;
 let name = "Jet";
@@ -46,12 +45,12 @@ let n3 = 30;
 let n4 = 40;
 //30Jet30404
 
-console.log(n1+n2+name+n3+n4);
+console.log(n1 + n2 + name + n3 + n4);
 
 console.log("--------------------------------");
 
 let age = 21;
-console.log(age.toString()+10);
+console.log(age.toString() + 10);
 let pi = 3.1415926535;
 console.log(pi.toFixed(5));
 
@@ -61,24 +60,9 @@ let name1 = "Wilson loves to eat noodles.";
 console.log(name1.length);
 console.log(name1[5]);
 console.log(name[100]);
-console.log(name1.slice(1,27));//1~26  (0~27) 故 頭尾都少1
+console.log(name1.slice(1, 27)); //1~26  (0~27) 故 頭尾都少1
 console.log(name1.indexOf("o"));
 console.log(name1.toUpperCase().indexOf("E"));
 console.log(name1.split(" "));
 console.log(name1.split(""));
-console.log("---------------//----------------");
-
-
-    
-let yourAge = prompt ("Enter  your age , please!");
-
-if (yourAge <= 12) {
-
-    alert("你的第一張兒童票 100 元");
-}else if((yourAge >12) &&( yourAge <= 65)) {
-    alert("你的票價是 350 元");
-}else{
-    alert("你的票價是 0 元");
-}
-
 console.log("---------------//----------------");
---
+++ html2021/j_002.js
@@ -0,0 +1,26 @@
+while (true) {
+    let yourAge = prompt("Enter  your age , please!");
+
+    yourAge = Number(yourAge);
+    console.log(yourAge, typeof yourAge);
+
+    if (isNaN(yourAge)) {
+        alert("GG! PLease English! Thankyou! ");
+        console.log(`your key-in words is${yourAge}`);
+    } else {
+        if (yourAge < 0) {
+            alert("請輸入正確的年齡!(年齡 需>= 0)");
+        } else if (yourAge <= 12) {
+            alert("你的第一張兒童票 100 元");
+            break;
+        } else if (yourAge > 12 && yourAge <= 65) {
+            alert("你的票價是 350 元");
+            break;
+        } else if (yourAge > 65 && yourAge < 150) {
+            alert("你的敬老票100 元");
+            break;
+        } else {
+            alert("請輸入正確年齡(<150)");
+        }
+    }
+}
